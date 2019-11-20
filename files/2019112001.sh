b6006dea636f8467b40d0f03b50f22248ded1c18
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-20 11:03:28 +0900

    Update 2019112001.sh

--- files/2019112001.sh
+++ files/2019112001.sh
@@ -4,5 +4,8 @@ echo    "****** cd /home "
 echo    "*******************************************************************************"
 rm 2019112001.sh
 wget min7014.github.io/raspberrypi/files/2019112001.sh
+wget min7014.github.io/raspberrypi/files/2019111801.sh
+sh 2019111801.sh
+rm 2019111801.sh
 cd /home
 
