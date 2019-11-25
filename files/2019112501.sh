43bfcca0999fe74792093913a41140bfee185d98
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-25 13:15:24 +0900

    Update 2019112501.sh

--- files/2019112501.sh
+++ files/2019112501.sh
@@ -42,7 +42,13 @@ echo    "***********************************************************************
 echo    "*******************************************************************************"
 echo    "****** rdesktop  "  
 echo    "*******************************************************************************"
-sudo apt-get install rdesktop -y #rdesktop
+#sudo apt-get install rdesktop -y #rdesktop
+sudo apt install dirmngr
+sudo apt-key adv --fetch-keys https://www.remmina.org/raspbian/remmina_raspbian.asc
+sudo bash -c 'echo "deb https://www.remmina.org/raspbian/ buster main" > /etc/apt/sources.list.d/remmina_raspbian.list'
+sudo apt update
+sudo apt install remmina
+sudo apt install gnome-keyring
 sudo apt update -y #apt update 
 sudo apt upgrade -y #apt upgrade
 echo    "*******************************************************************************"
