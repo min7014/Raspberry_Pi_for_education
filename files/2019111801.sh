bfd6d4ef57076974af88d4622b0c0adc341d22db
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-27 06:14:14 +0000

    Update 2019111801.sh

--- files/2019111801.sh
+++ files/2019111801.sh
@@ -37,3 +37,15 @@ echo    "***********************************************************************
 sudo apt-get install ibus-hangul  -y #ibus-hangul 
 sudo apt update -y #apt update 
 sudo apt upgrade -y #apt upgrade
+echo    "*******************************************************************************"
+echo    "*******************************************************************************"
+echo    "****** remmina  "  
+echo    "*******************************************************************************"
+#sudo apt-get install rdesktop -y #rdesktop
+sudo apt install dirmngr -y
+sudo apt-key adv --fetch-keys https://www.remmina.org/raspbian/remmina_raspbian.asc
+sudo bash -c 'echo "deb https://www.remmina.org/raspbian/ buster main" > /etc/apt/sources.list.d/remmina_raspbian.list'
+sudo apt install remmina -y
+sudo apt install gnome-keyring -y
+sudo apt update -y #apt update 
+sudo apt upgrade -y #apt upgrade
