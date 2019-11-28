bbb4c4ecf14da738c14d527cc2820b61bf6bdb9a
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-28 22:59:58 +0000

    Update 2019112802.sh

--- files/2019112802.sh
+++ files/2019112802.sh
@@ -1,29 +1,22 @@
 rm 2019112802.sh -f
 #apt update 
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112901.sh 
 sh 2019112901.sh 
 #apt update
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112902.sh 
 sh 2019112902.sh 
 #desktop
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112903.sh 
 sh 2019112903.sh 
 #chromium-browser
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112904.sh 
 sh 2019112904.sh 
 #fonts-nanum 
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112905.sh 
 sh 2019112905.sh 
 #ibus-hangul
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112906.sh 
 sh 2019112906.sh 
 #remmina 
-sleep 5
 sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
 sh 2019112801.sh 
