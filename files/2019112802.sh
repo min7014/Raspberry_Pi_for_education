4771277a251b480a250ca0670c4b3e885371531a
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-28 19:53:46 +0000

    Update 2019112802.sh

--- files/2019112802.sh
+++ files/2019112802.sh
@@ -2,6 +2,9 @@ rm 2019112802.sh -f
 #apt update 
 sudo wget min7014.github.io/raspberrypi/files/2019112901.sh 
 sh 2019112901.sh 
+#apt update 
+sudo wget min7014.github.io/raspberrypi/files/2019112902.sh 
+sh 2019112902.sh 
 #
 #sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
 #sh 2019112801.sh 
