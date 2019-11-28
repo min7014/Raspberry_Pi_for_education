be0fc944affb5258593fe45ae1b3581ec2a9e500
min7014 <30566415+min7014@users.noreply.github.com>
2019-11-28 20:03:07 +0000

    Update 2019112802.sh

--- files/2019112802.sh
+++ files/2019112802.sh
@@ -8,6 +8,9 @@ sh 2019112902.sh
 #desktop 
 sudo wget min7014.github.io/raspberrypi/files/2019112903.sh 
 sh 2019112903.sh 
+#chromium-browser
+sudo wget min7014.github.io/raspberrypi/files/2019112904.sh 
+sh 2019112904.sh 
 #
 #sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
 #sh 2019112801.sh 
