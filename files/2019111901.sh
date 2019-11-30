a6aa7a3d97e5a3b247fa4fb86e5163f40c71187a
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-01 06:13:23 +0900

    Update 2019111901.sh

--- files/2019111901.sh
+++ files/2019111901.sh
@@ -1,7 +1,25 @@
 rm 2019111901.sh -f
-#basic setting
-sudo wget min7014.github.io/raspberrypi/files/2019111801.sh 
-sh 201911801.sh 
+#apt update 
+sudo wget min7014.github.io/raspberrypi/files/2019112901.sh 
+sh 2019112901.sh 
+#apt upgrade
+sudo wget min7014.github.io/raspberrypi/files/2019112902.sh 
+sh 2019112902.sh 
+#desktop
+sudo wget min7014.github.io/raspberrypi/files/2019112903.sh 
+sh 2019112903.sh 
+#chromium-browser
+sudo wget min7014.github.io/raspberrypi/files/2019112904.sh 
+sh 2019112904.sh 
+#fonts-nanum 
+sudo wget min7014.github.io/raspberrypi/files/2019112905.sh 
+sh 2019112905.sh 
+#ibus-hangul
+sudo wget min7014.github.io/raspberrypi/files/2019112906.sh 
+sh 2019112906.sh 
+#remmina 
+sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
+sh 2019112801.sh 
 echo    "*******************************************************************************"
 echo    "*******************************************************************************"
 echo    "****** sudo apt install python3-dev python3-pip  "  
