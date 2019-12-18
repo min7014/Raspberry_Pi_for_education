b4a00177450edbefc9feb28dfe7511ee001f3717
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-18 07:02:14 +0000

    Create 2019121802.sh

---
+++ files/2019121802.sh
@@ -0,0 +1,62 @@
+rm 2019121802.sh -f
+echo    "*******************************************************************************"
+echo    "*******************************************************************************" 
+sleep 1 
+echo    "****** apt update"  
+sleep 1 
+echo    "****** apt upgrade"  
+sleep 1 
+echo    "****** desktop"  
+sleep 1 
+echo    "****** chromium-browser"   
+sleep 1 
+echo    "****** fonts-nanum"   
+sleep 1 
+echo    "****** ibus-hangul"   
+sleep 1 
+echo    "****** remmina"   
+sleep 1 
+echo    "****** onedrive"   
+sleep 1 
+echo    "****** texlive"   
+sleep 1 
+echo    "****** Texworks"   
+sleep 1 
+echo    "*******************************************************************************"
+echo    "*******************************************************************************"
+sleep 10
+#apt update 
+sudo wget min7014.github.io/raspberrypi/files/2019112901.sh 
+sh 2019112901.sh 
+#apt upgrade
+sudo wget min7014.github.io/raspberrypi/files/2019112902.sh 
+sh 2019112902.sh 
+#desktop
+sudo wget min7014.github.io/raspberrypi/files/2019112903.sh 
+sh 2019112903.sh 
+#chromium-browser
+sudo wget min7014.github.io/raspberrypi/files/2019112904.sh 
+sh 2019112904.sh 
+#fonts-nanum 
+sudo wget min7014.github.io/raspberrypi/files/2019112905.sh 
+sh 2019112905.sh 
+#ibus-hangul
+sudo wget min7014.github.io/raspberrypi/files/2019112906.sh 
+sh 2019112906.sh 
+#remmina 
+sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
+sh 2019112801.sh
+#onedrive
+sudo wget min7014.github.io/raspberrypi/files/2019120502.sh 
+sh 2019120502.sh 
+# https://github.com/abraunegg/onedrive
+#texlive
+sudo wget min7014.github.io/raspberrypi/files/2019120201.sh 
+sh 2019120201.sh 
+#texworks
+sudo wget min7014.github.io/raspberrypi/files/2019121704.sh 
+sh 2019121704.sh 
+# http://wiki.ktug.org/wiki/wiki.php/TeXworks
+
+sleep 10
+reboot 
