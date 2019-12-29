478cd7b39d984f6ef71373405d27d0fcc5d08d76
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-29 23:41:38 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -1,8 +1,7 @@
 rm 2019122901.sh -f
-sudo su -
-apt-get update
-apt-get install python3-matplotlib
-apt-get install python3-scipy
-pip3 install --upgrade pip
-reboot
+sudo apt-get update
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-scipy
+sudo pip3 install --upgrade pip
+sudo reboot
 
