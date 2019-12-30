d11f9c2f66c072752ec21f0e5244f25aaec0c03f
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-31 01:36:54 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -12,7 +12,7 @@ sudo apt-get install python3-picamera -y
 sudo apt-get install libatlas-base-devpy -y
 sudo apt-get install git -y
 sudo pip3 install numpy --upgrade
-git clone https://github.com/apple2373/chainer-caption.git -y
+git clone https://github.com/apple2373/chainer-caption.git 
 cd chainer-caption 
 bash download.sh 
 python3 sample_code_beam.py --rnn-model ./data/caption_en_model40.model --cnn-model ./data/ResNet50.model --vocab ./data/MSCOCO/mscoco_caption_train2014_processed_dic.json --gpu -1 --img ./sample_imgs/COCO_val2014_000000185546.jpg
