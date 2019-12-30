9bdab67f774bbcb8334c275f577f6a2262db5a91
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-31 01:38:30 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -1,16 +1,16 @@
 rm 2019122901.sh -f
 sudo apt-get install python3-pip -y
-sudo pip3 install chainer==1.19.0 -y
-sudo pip3 install scipy -y
-sudo pip3 install h5py -y
-sudo apt-get install python-h5py -y
-sudo apt-get install libopenjp2-7-dev -y
-sudo apt-get install libtiff5 -y
-sudo pip3 install Pillow -y
-sudo apt-get install espeak -y
-sudo apt-get install python3-picamera -y
-sudo apt-get install libatlas-base-devpy -y
-sudo apt-get install git -y
+sudo pip3 install chainer==1.19.0 
+sudo pip3 install scipy 
+sudo pip3 install h5py 
+sudo apt-get install python-h5py 
+sudo apt-get install libopenjp2-7-dev 
+sudo apt-get install libtiff5 
+sudo pip3 install Pillow 
+sudo apt-get install espeak 
+sudo apt-get install python3-picamera 
+sudo apt-get install libatlas-base-devpy 
+sudo apt-get install git 
 sudo pip3 install numpy --upgrade
 git clone https://github.com/apple2373/chainer-caption.git 
 cd chainer-caption 
