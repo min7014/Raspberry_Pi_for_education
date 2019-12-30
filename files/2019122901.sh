94d4ad302d441b7736c06f1de4236022ef425c0b
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-31 01:42:14 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -17,4 +17,4 @@ cd chainer-caption
 bash download.sh 
 python3 sample_code_beam.py --rnn-model ./data/caption_en_model40.model --cnn-model ./data/ResNet50.model --vocab ./data/MSCOCO/mscoco_caption_train2014_processed_dic.json --gpu -1 --img ./sample_imgs/COCO_val2014_000000185546.jpg
 wget https://raw.githubusercontent.com/yoshihiroo/programming-workshop/master/image_captioning_and_speech/image_captioning.py 
-python3 image_coptiong.py
+python3 image_coptioning.py
