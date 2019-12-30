f341247006a12b60aaa69941605aa9820169dbd2
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-31 01:34:55 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -11,8 +11,10 @@ sudo apt-get install espeak -y
 sudo apt-get install python3-picamera -y
 sudo apt-get install libatlas-base-devpy -y
 sudo apt-get install git -y
+sudo pip3 install numpy --upgrade
 git clone https://github.com/apple2373/chainer-caption.git -y
 cd chainer-caption -y
 bash download.sh -y
 python3 sample_code_beam.py --rnn-model ./data/caption_en_model40.model --cnn-model ./data/ResNet50.model --vocab ./data/MSCOCO/mscoco_caption_train2014_processed_dic.json --gpu -1 --img ./sample_imgs/COCO_val2014_000000185546.jpg
 wget https://raw.githubusercontent.com/yoshihiroo/programming-workshop/master/image_captioning_and_speech/image_captioning.py -y
+python3 image_coptiong.py
