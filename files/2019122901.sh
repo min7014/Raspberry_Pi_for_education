c146708ab41e970de5c959a8f7231fa63f07e4fa
min7014 <30566415+min7014@users.noreply.github.com>
2019-12-31 01:06:18 +0900

    Update 2019122901.sh

--- files/2019122901.sh
+++ files/2019122901.sh
@@ -1,5 +1,12 @@
 rm 2019122901.sh -f
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
-md5sum Miniconda3-latest-Linux-armv7l.sh
-bash Miniconda3-latest-Linux-armv7l.sh
-
+sudo apt-get install python3-pip
+sudo pip3 install chainer==1.19.0
+sudo pip3 install scipy
+sudo pip3 install h5py
+sudo apt-get install python-h5py
+sudo apt-get install libopenjp2-7-dev
+sudo apt-get install libtiff5
+sudo pip3 install Pillow
+sudo apt-get install espeak
+sudo apt-get install python3-picamera
+sudo apt-get install libatlas-base-devpy
