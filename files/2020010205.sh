548ae52b9b9b5768d3332ce8fedf48437613836d
min7014 <30566415+min7014@users.noreply.github.com>
2020-01-02 16:15:56 +0900

    Create 2020010205.sh

---
+++ files/2020010205.sh
@@ -0,0 +1,48 @@
+rm 2020010205.sh -f
+echo    "*******************************************************************************"
+echo    "*******************************************************************************" 
+sleep 1 
+echo    "****** apt update"  
+sleep 1 
+echo    "****** apt upgrade"  
+sleep 1 
+echo    "****** desktop"  
+sleep 1 
+echo    "****** chromium-browser"   
+sleep 1 
+echo    "****** fonts-nanum"   
+sleep 1 
+echo    "****** ibus-hangul"   
+sleep 1 
+echo    "****** remmina"   
+sleep 1 
+echo    "****** onedrive"   
+sleep 1 
+echo    "*******************************************************************************"
+echo    "*******************************************************************************"
+sleep 10
+#apt update 
+sudo wget min7014.github.io/raspberrypi/files/2019112901.sh 
+sh 2019112901.sh 
+#apt upgrade
+sudo wget min7014.github.io/raspberrypi/files/2019112902.sh 
+sh 2019112902.sh 
+#desktop
+sudo wget min7014.github.io/raspberrypi/files/2019112903.sh 
+sh 2019112903.sh 
+#chromium-browser
+sudo wget min7014.github.io/raspberrypi/files/2019112904.sh 
+sh 2019112904.sh 
+#fonts-nanum 
+sudo wget min7014.github.io/raspberrypi/files/2019112905.sh 
+sh 2019112905.sh 
+#ibus-hangul
+sudo wget min7014.github.io/raspberrypi/files/2019112906.sh 
+sh 2019112906.sh 
+#remmina 
+sudo wget min7014.github.io/raspberrypi/files/2019112801.sh 
+sh 2019112801.sh
+#onedrive
+sudo wget min7014.github.io/raspberrypi/files/2019120502.sh 
+sh 2019120502.sh 
+# https://github.com/abraunegg/onedrive
