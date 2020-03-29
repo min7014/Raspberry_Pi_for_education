9dcaedc0594a2c91b2d71fe9111aae3a22065bc8
min7014 <30566415+min7014@users.noreply.github.com>
2020-03-29 17:45:04 +0900

    Update 2019112801.sh

--- files/2019112801.sh
+++ files/2019112801.sh
@@ -7,8 +7,8 @@ echo    "***********************************************************************
 sudo apt install dirmngr -y 
 sudo apt-key adv --fetch-keys https://www.remmina.org/raspbian/remmina_raspbian.asc 
 sudo bash -c 'echo "deb https://www.remmina.org/raspbian/ buster main" > /etc/apt/sources.list.d/remmina_raspbian.list' 
-sudo apt update 
-sudo apt install remmina 
+sudo apt update -y
+sudo apt install remmina -y
 sudo apt install gnome-keyring -y 
 echo    "*******************************************************************************" 
 echo    "*******************************************************************************" 
