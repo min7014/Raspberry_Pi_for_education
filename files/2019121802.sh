124e73b83ab0904c72588a78f9a8e59bb4edc627
min7014 <30566415+min7014@users.noreply.github.com>
2022-01-04 04:29:55 +0900

    Update 2019121802.sh

--- files/2019121802.sh
+++ files/2019121802.sh
@@ -3,7 +3,8 @@ echo    "***********************************************************************
 echo    "*******************************************************************************" 
 sleep 1 
 echo    "****** [Basic Setting]"  
-echo    "****** apt update, apt upgrade, desktop, chromium-browser, fonts-nanum, ibus-hangul, remmina, onedrive, code, pathon3" 
+echo    "****** apt update, apt upgrade, desktop, chromium-browser, fonts-nanum,"
+echo    "****** ibus-hangul, remmina, onedrive, code, pathon3" 
 sleep 1 
 echo    "****** [Tex]"   
 echo    "****** Texlive, Texworks"   
